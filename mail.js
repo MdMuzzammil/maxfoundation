c5c8a2a71cfd1a6ac584f1fd0d9424e9bc1b338f
HeyZammi <140315470+HeyZammi@users.noreply.github.com>
2023-08-31 17:48:05 +0530

    commit

--- mail.js
+++ mail.js
@@ -1,22 +1,30 @@
 const firebaseConfig = {
-   
+    apiKey: "AIzaSyDLkgaj98pMxFZzqQ2is2s6J6s_dDIzBNA",
+    authDomain: "max-foundation-ranchi.firebaseapp.com",
+    databaseURL: "https://max-foundation-ranchi-default-rtdb.firebaseio.com",
+    projectId: "max-foundation-ranchi",
+    storageBucket: "max-foundation-ranchi.appspot.com",
+    messagingSenderId: "459914794262",
+    appId: "1:459914794262:web:1b23d444e5c252ab1d2c37",
+    measurementId: "G-4745MQ51V0"
   };
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  var enquiryFormDB = firebase.database().ref("enquiryForm");
+  let enquiryFormDB = firebase.database().ref("enquiryForm");
   
   document.getElementById("enquiry-form").addEventListener('submit', submitForm);
     
   function submitForm(e){
+    console.log('submitted')
     e.preventDefault();
 
-    var FullName= getElementVal("name");
-    var Contact= getElementVal("contactNumber");
-    var College= getElementVal("college");
-    var Stream= getElementVal("stream");
-    var CurrentClass = getElementVal("class");
-    var Email = getElementVal("email");
+    let FullName= getElementVal("name");
+    let Contact= getElementVal("contactNumber");
+    let College= getElementVal("college");
+    let Stream= getElementVal("stream");
+    let CurrentClass = getElementVal("class");
+    let Email = getElementVal("email");
 
     if((Contact.length<10) || (Contact.length>10)){
     alert("Please Enter a Valid Contact Number")
@@ -38,18 +46,19 @@ const firebaseConfig = {
     // console.log(FullName + " " + Contact + " " + College+ " " + Stream+ " " + CurrentClass)
     saveData(FullName, Contact, College, Email, Stream, CurrentClass);
     document.getElementById("enquiry-form").reset();
-    document.querySelector('.alert').style.display='block';
+    // document.querySelector('.alert').style.display='block';
     alert("Your Form Has Been Submitted")
-    setTimeout(()=>{
-        // document.querySelector('.alert').style.display='none';
-    }, 5000);
+    // setTimeout(()=>{
+    //     // document.querySelector('.alert').style.display='none';
+    // }, 5000)
+    ;
 }
 const getElementVal = (id) => {
   return document.getElementById(id).value;
 }
 
   function saveData(FullName, Contact, College, Email, Stream, CurrentClass){
-    var newEnquiryData = enquiryFormDB.push();
+    let newEnquiryData = enquiryFormDB.push();
     newEnquiryData.set({
         FullName: FullName,
         Contact: Contact,
