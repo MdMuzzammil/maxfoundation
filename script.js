1efefb1480086d1e36f7611c8de288e45519dbe5
HeyZammi <140315470+HeyZammi@users.noreply.github.com>
2023-09-03 12:25:16 +0530

    commit

--- script.js
+++ script.js
@@ -147,15 +147,27 @@ let mobLogo = document.querySelector('.mobile-logo');
 let mobLogoP = document.querySelector('.mobile-logo-parent');
 
 hamburger.addEventListener('click', (element) => {
-    if(itemList[1].style.display != 'none'){
-        itemList.forEach((element) => {element.style.display = 'none';element.style.transition='1s'});
-
-        mobLogo.style.display= 'block';
-        mobLogoP.style.display= 'block';
-    }
-    else{
+    if(itemList[1].style.display != 'block'){
         itemList.forEach((element) => {element.style.display = 'block'});
         mobLogo.style.display= 'none';
         mobLogoP.style.display= 'none';
     }
+    else{
+        itemList.forEach((element) => {element.style.display = 'none';element.style.transition='1s'});
+
+        mobLogo.style.display= 'block';
+        mobLogoP.style.display= 'block';
+        
+    }
+    // if(itemList[1].style.display != 'none'){
+    //     itemList.forEach((element) => {element.style.display = 'none';element.style.transition='1s'});
+
+    //     mobLogo.style.display= 'block';
+    //     mobLogoP.style.display= 'block';
+    // }
+    // else{
+    //     itemList.forEach((element) => {element.style.display = 'block'});
+    //     mobLogo.style.display= 'none';
+    //     mobLogoP.style.display= 'none';
+    // }
 })
